205ed892b44c927f51ff8bdd0070e4d9ade501c2
Connor McCormick <mrredcon@gmail.com>
2023-11-06 00:27:14 -0600

    Add types in xfce config

--- xfce/install.sh
+++ xfce/install.sh
@@ -4,9 +4,9 @@ set -e
 DIR=$(dirname "$(readlink -f "$0")")
 
 clock_path=$(xfconf-query -c xfce4-panel -lv | grep clock | awk '{print $1}')
-xfconf-query -c xfce4-panel -p "$clock_path/digital-time-format" -s '%A %m/%d, %I:%M %p'
-xfconf-query -c xfce4-panel -p "$clock_path/digital-layout" -s '3'
-xfconf-query -c xfce4-panel -p "$clock_path/digital-time-font" -s 'Sans 10'
+xfconf-query -c xfce4-panel --create -t 'string' -p "$clock_path/digital-time-format" -s '%A %m/%d, %I:%M %p'
+xfconf-query -c xfce4-panel --create -t 'uint' -p "$clock_path/digital-layout" -s '3'
+xfconf-query -c xfce4-panel --create -t 'string' -p "$clock_path/digital-time-font" -s 'Sans 10'
 echo 'Configured xfce panel clock'
 
 mkdir -p ~/.config/xfce4/terminal
