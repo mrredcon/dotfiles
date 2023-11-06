37ee4bc472e2b4861c6ea8e0234aafaaedb086aa
Connor McCormick <mrredcon@gmail.com>
2023-11-06 10:00:14 -0600

    Add bringToFront shortcut

--- xfce/install.sh
+++ xfce/install.sh
@@ -9,5 +9,7 @@ xfconf-query -c xfce4-panel --create -t 'uint' -p "$clock_path/digital-layout" -
 xfconf-query -c xfce4-panel --create -t 'string' -p "$clock_path/digital-time-font" -s 'Sans 10'
 echo 'Configured xfce panel clock'
 
+xfconf-query -c xfce4-keyboard-shortcuts --create -p '/commands/custom/<Alt>backslash' -s "$HOME/bin/bringToFront xfce4-terminal" -t 'string' && echo 'Added bringToFront shortcut'
+
 mkdir -p ~/.config/xfce4/terminal
 cp $DIR/terminalrc ~/.config/xfce4/terminal/ && echo 'Installed xfce terminalrc'
