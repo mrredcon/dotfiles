fede6e290a28734de770ea3e0d0286238b8b0d95
Connor McCormick <mrredcon@gmail.com>
2023-11-05 23:49:34 -0600

    Configure xfce clock differently

--- xfce/install.sh
+++ xfce/install.sh
@@ -3,8 +3,11 @@ set -e
 
 DIR=$(dirname "$(readlink -f "$0")")
 
-mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml
-cp $DIR/xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/ && echo 'Installed xfce4-panel.xml'
+clock_path=$(xfconf-query -c xfce4-panel -lv | grep clock | awk '{print $1}')
+xfconf-query -c xfce4-panel -p "$clock_path/digital-time-format" -s '%A %m/%d, %I:%m %p'
+xfconf-query -c xfce4-panel -p "$clock_path/digital-layout" -s '3'
+xfconf-query -c xfce4-panel -p "$clock_path/digital-time-font" -s 'Sans 10'
+echo 'Configured xfce panel clock'
 
 mkdir -p ~/.config/xfce4/terminal
 cp $DIR/terminalrc ~/.config/xfce4/terminal/ && echo 'Installed xfce terminalrc'
