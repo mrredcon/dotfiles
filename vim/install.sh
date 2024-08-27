2229749bddf071ce3d13949b4af21ea9796d4de4
Connor McCormick <mccormick.connor@heb.com>
2024-08-27 14:02:29 -0500

    Add Linux check in vim install, add local vimrc

--- vim/install.sh
+++ vim/install.sh
@@ -6,9 +6,12 @@ DIR=$(dirname "$(readlink -f "$0")")
 mkdir -p ~/.vim/colors
 ln -s "$DIR/solarized.vim" ~/.vim/colors && echo 'Installed solarized vim colorscheme'
 
-# Download vim-fakeclip so we can copy/paste in Wayland
-mkdir -p ~/.vim/pack/vendor/start
-git clone --depth 1 https://github.com/kana/vim-fakeclip ~/.vim/pack/vendor/start/fakeclip
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+	# Download vim-fakeclip so we can copy/paste in Wayland
+	mkdir -p ~/.vim/pack/vendor/start
+	git clone --depth 1 https://github.com/kana/vim-fakeclip ~/.vim/pack/vendor/start/fakeclip
+fi
 
 # Check if running Arch
 if [ ! -f "/etc/arch-release" ]; then
