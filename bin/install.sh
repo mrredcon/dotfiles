9695f1fc03e3301c70a340c3f885ca10e948ee2f
Connor McCormick <mrredcon@gmail.com>
2025-02-02 14:41:25 -0600

    Edit bin install script

--- bin/install.sh
+++ bin/install.sh
@@ -5,4 +5,7 @@ set -e
 DIR=$(dirname "$(readlink -f "$0")")
 
 mkdir -p ~/bin
-ln -s $DIR/bringToFront ~/bin && echo 'Installed bringToFront'
+ln -f -s $DIR/bringToFront ~/bin && echo 'Installed bringToFront'
+ln -f -s $DIR/pwgen_alphanumeric ~/bin && echo 'Installed pwgen_alphanumeric'
+ln -f -s $DIR/pwgen_printable ~/bin && echo 'Installed pwgen_printable'
+
