338287a021bbb6ca942ab719002c9ceebf430c84
Connor McCormick <mccormick.connor@heb.com>
2025-02-07 16:13:04 -0600

    Add urlencode script, add dotfiles bin to PATH

--- bin/install.sh
+++ bin/install.sh
@@ -6,6 +6,4 @@ DIR=$(dirname "$(readlink -f "$0")")
 
 mkdir -p ~/bin
 ln -f -s $DIR/bringToFront ~/bin && echo 'Installed bringToFront'
-ln -f -s $DIR/pwgen_alphanumeric ~/bin && echo 'Installed pwgen_alphanumeric'
-ln -f -s $DIR/pwgen_printable ~/bin && echo 'Installed pwgen_printable'
 
