bc0822c4735d1ac2e4f5230a0f4cd65ac35cf148
Connor McCormick <mrredcon@gmail.com>
2026-03-20 15:27:57 -0500

    Add Darwin check to osx script, Dock speed

--- osx/install.sh
+++ osx/install.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if [[ $(uname) != "Darwin" ]]; then
+	exit
+fi
+
 defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder
 defaults write -g InitialKeyRepeat 25
 defaults write -g KeyRepeat 2
+
+defaults write com.apple.dock autohide-delay -float 0 && defaults write com.apple.dock autohide-time-modifier -float 0.4 && killall Dock
